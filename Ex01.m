6b703cb4c25550e0ca10059a577b98425179145b
alvesouza <36734373+alvesouza@users.noreply.github.com>
2019-04-22 12:00:11 -0300

    Add files via upload

---
+++ Ex01.m
@@ -0,0 +1,17 @@
+G = zpk([],[-3],[1])
+F = zpk([-5],[-1],[1])
+H = zpk([],[0],[1])
+T = F + feedback(G*H,1,1)
+%%
+%K no serie
+rlocus(-G*H)
+
+%%
+%para K no paralelo não muda
+
+%%
+%com polo variante em G
+J = zpk([0],[-1 1],[3])
+%%
+%com polo variante em H
+J = zpk([0],[-1 1],[3])
